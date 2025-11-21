589272be15b9db30be1c514aa4c85e1190566297
Dawid Paluch <dawid1paluch@gmail.com>
2025-11-21 14:52:05 +0000

    terrain includes octaves

--- Assets/generalTerrainGenerationScript.cs
+++ Assets/generalTerrainGenerationScript.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 [RequireComponent(typeof(Terrain))]
@@ -7,18 +8,19 @@ public class RandomTerrain : MonoBehaviour
     [SerializeField] protected float noiseScale = 10f;
     [SerializeField] protected float heightMultiplier = 0.03f;
     [SerializeField] protected int seed = 0;
+    [SerializeField] protected int octaves = 4;
+    [SerializeField] protected float persistance = 0.5f;
+    [SerializeField] protected float lacunarity = 2f;
 
     // References to Terrain and TerrainData components data
     protected Terrain terrain;
     protected TerrainData terrainData;
-    protected Renderer textureRenderer;
 
     // Initialize terrain generation on Awake
     private void Awake()
     {
         terrain = GetComponent<Terrain>();
         terrainData = terrain.terrainData;
-        textureRenderer = GetComponent<Renderer>();
 
         // Call terrain generation method
         GenerateTerrain(this.terrain, this.terrainData, this.noiseScale, this.heightMultiplier, this.seed);
@@ -33,6 +35,7 @@ public class RandomTerrain : MonoBehaviour
             //seed = Random.Range(0, 1000);
             GenerateTerrain(this.terrain, this.terrainData, this.noiseScale, this.heightMultiplier, this.seed);
         }
+        GenerateTerrain(this.terrain, this.terrainData, this.noiseScale, this.heightMultiplier, this.seed);
     }
 
     // Main terrain generation method using Perlin noise
@@ -41,7 +44,7 @@ public class RandomTerrain : MonoBehaviour
         int heightmapWidth = passedTerrainData.heightmapResolution;
         int heightmapHeight = passedTerrainData.heightmapResolution;
 
-        float[,] noiseMap = GenerateNoiseMap(heightmapWidth, heightmapHeight, passedNoiseScale, passedSeed);
+        float[,] noiseMap = GenerateNoiseMap(heightmapWidth, heightmapHeight, passedNoiseScale, octaves, persistance, lacunarity, passedSeed);
 
         ColourTerrain(noiseMap);
 
@@ -64,27 +67,70 @@ public class RandomTerrain : MonoBehaviour
     }
 
     // Method to generate a noise map - can be used for heightmaps or other procedural generation needs
-    protected float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float mapScale, int mapSeed)
+    protected float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float mapScale, int octaves, float persistance, float lacunarity, int mapSeed)
     {
         if (mapScale <= 0)
         {
             mapScale = 0.0001f;
         }
 
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
         float[,] noiseMap = new float[mapHeight, mapWidth];
-        Random.InitState(mapSeed);
-        Vector2 randomOffset = new(Random.Range(-10000f, 10000f), Random.Range(-10000f, 10000f));
+
+        System.Random random = new System.Random(mapSeed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = random.Next(-100000, 100000);
+            float offsetY = random.Next(-100000, 100000);
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
+
+        //Random.InitState(mapSeed);
+        //Vector2 randomOffset = new(Random.Range(-10000f, 10000f), Random.Range(-10000f, 10000f));
 
         for (int y = 0; y < mapHeight; y++)
         {
             for (int x = 0; x < mapWidth; x++)
             {
-                float xCoord = (float)x / mapWidth * mapScale+ randomOffset.x;
-                float yCoord = (float)y / mapHeight * mapScale + randomOffset.y;
-                float sample = Mathf.PerlinNoise(xCoord, yCoord);
-                noiseMap[y, x] = sample;
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+                    float xCoord = (x / mapScale) * frequency + octaveOffsets[i].x;
+                    float yCoord = (y / mapScale) * frequency + octaveOffsets[i].y;
+
+                    float noiseHeightI = Mathf.PerlinNoise(xCoord, yCoord) * 2 - 1;
+                    noiseHeight += noiseHeightI * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[y, x] = noiseHeight;
             }
         }
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                noiseMap[y, x] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[y, x]);
+            }
+        }
+
         return noiseMap;
     }
 
