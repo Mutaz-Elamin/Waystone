bbbfe8dc7004cc0d2bc02a83d84a9b3bb8c7dc45
Mahdi <mohammed.mahdi.taihi@outlook.com>
2025-11-26 18:12:14 +0000

    Attack Animation + Collider Working

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -2,38 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    [Header("References")]
-    public Animator animator;      
-    public SwordHitbox swordHitbox; 
+    public Animator animator;
+    public SwordHitbox swordHitbox;
 
-    [Header("Attack Cooldowns")]
-    public float lightCooldown = 0.3f;
-    public float heavyCooldown = 0.7f;
+    public float attackActiveTime = 0.25f;
+    public float attackCooldown = 0.5f;
 
-    private float lastLightTime = -999f;
-    private float lastHeavyTime = -999f;
+    private bool canAttack = true;
 
     public void LightAttack()
     {
-        if (Time.time - lastLightTime < lightCooldown) return;
+        if (!canAttack) return;
+        StartCoroutine(PerformAttack());
+    }
 
-        lastLightTime = Time.time;
+    private IEnumerator PerformAttack()
+    {
+        canAttack = false;
 
         animator.SetTrigger("LightAttack");
-        swordHitbox.canHit = true; 
-    }
 
-    public void HeavyAttack()
-    {
-        if (Time.time - lastHeavyTime < heavyCooldown) return;
-
-        lastHeavyTime = Time.time;
-
-        animator.SetTrigger("HeavyAttack");
+        // Enable hitbox
         swordHitbox.canHit = true;
+        swordHitbox.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(attackActiveTime);
+
+        // Disable hitbox
+        swordHitbox.canHit = false;
+        swordHitbox.gameObject.SetActive(false);
+
+        // Wait for cooldown
+        yield return new WaitForSeconds(attackCooldown);
+        canAttack = true;
     }
+
+    //public void HeavyAttack()
+
+    // { if (Time.time - lastHeavyTime < heavyCooldown) return;
+
+    //lastHeavyTime = Time.time;
+
+    //  animator.SetTrigger("HeavyAttack");
+    //    swordHitbox.canHit = true;}
+
 }
+
